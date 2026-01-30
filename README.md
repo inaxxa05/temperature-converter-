5e8eca42150ca53d9e23b484d9e28e4468557e70
Ina <inabilinska@interia.eu>
2026-01-30 12:06:08 +0100

    Adds Rankine temperature conversion function, - Implements celsius_to_rankine() conversion - Implements rankine_to_celsius() conversion - Updates test cases with the new Rankine examples - All conversions tested and verified.

---
+++ README.md
@@ -0,0 +1,40 @@
+
+# Temperature Converter
+
+A simple Python application that converts temperature between Celsius, Fahrenheit and Kelvin.
+
+
+## Installation
+
+1. Clone this repo:
+  ```git clone <your-repo-url>
+     cd temperature-converter
+  ```
+
+2. Create a virtual enviroment
+  - python3 -m venv venv
+  - source venv/bin/activate # macOS/Linux
+  - venv\Scripts\activate # Windows
+
+3. Install dependencies:
+  -pip install -r requirements.txt
+
+
+## Usage
+
+```
+python src/temp_app/converter.py
+```
+
+
+
+## Features
+- Convert Celsius ↔️ Fahrenheit
+- Convert Celsius ↔️ Kelvin
+- Clean, documented code
+- Professional project structure
+
+### Author
+Ina Bilinska
+
+
---
+++ src/temp_app/converter.py
@@ -0,0 +1,55 @@
+"""
+Temperature Converter Module 
+Provides functions to convert between Celcius, Fahrenheit and Kelvin. 
+"""
+def celsius_to_fahrenheit(celsius): 
+	"""
+	Converts temperature from Celcius to Fahrenheit.
+	"""
+	return (celsius*9/5)+32
+
+
+def fahrenheit_to_celsius(fahrenheit):
+	"""Converts temperature from Fahrenheit to Celcius.
+	"""
+	return (fahrenheit - 32) * 5/9
+
+
+def celsius_to_kelvin(celsius):
+	"""
+	Converts temperture from Celsius to Kelvin
+	"""
+	return celsius + 273.15
+
+
+def kelvin_to_celsius(kelvin):
+	"""
+	Converts temperture from Kelvin to Celsius.
+	"""
+	return kelvin - 273.15
+
+def celsius_to_rankine(celsius):
+	""" 
+	Convert temperature from celsius to Rankine.
+	"""
+	kelvin = celsius_to_kelvin(celsius)
+	return kelvin * 9/5
+
+def rankine_to_celsius(rankine):
+	"""
+	Converts temperature from Rankine to Celsius. 
+	"""
+	kelvin = rankine * 5/9 
+	return kelvin_to_celsius(kelvin)
+
+
+if __name__ == "__main__":
+
+    print("Temperature Converter Tests")
+    print(f"0°C = {celsius_to_rankine(0):.2f}°R") 
+    print(f"491.67°R = {rankine_to_celsius(491.67):.2f}°C")
+    print(f'40°C = {celsius_to_fahrenheit(40)}°F')
+    print(f'105°F = {round(fahrenheit_to_celsius(105),2)}°C')
+    print(f'200°K = {round(kelvin_to_celsius(200),2)}K')
+
+
